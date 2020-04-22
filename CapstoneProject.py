17ffd0feb07255d1330cc1810d1604f1b3b01218
NISHCHAIY PATEL <nishchaiy.patel@in.ibm.com>
2020-04-22 11:33:31 +0530

    new python code to print capstone project

---
+++ CapstoneProject.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project
+
+# ### Hello...!!! I am Nishchaiy Patel and this is my project for Capstone
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
